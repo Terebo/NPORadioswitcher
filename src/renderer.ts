22c2392b8ecc8d0fe0684cd741db9f03e75a8abf
Terebo <63092966+Terebo@users.noreply.github.com>
2021-05-09 21:40:42 +0200

    more groundwork for displaying the timetable

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,8 +1,40 @@
 var settings = require("electron-settings");
 var ipcRenderer = require("electron").ipcRenderer;
 const fs = require("fs");
-const stations: unknown = JSON.parse(fs.readFileSync(__dirname + "/dat/stations.json", {encoding: "utf8"}));
+const path = require("path");
+const stations: stations = JSON.parse(fs.readFileSync(__dirname + "/dat/stations.json", {encoding: "utf8"}));
 
+/* === interfaces === */
+
+interface stations { 
+    "NPORadio1": stationsType,
+    "NPORadio2": stationsType,
+    "NPO3FM": stationsType,
+    "NPORadio4": stationsType,
+    "NPORadio5": stationsType,
+    "NPOFunX": stationsType,
+    "2S&J": stationsType,
+    "3FMalternative": stationsType,
+    "3FMkx": stationsType,
+    "4Concerten": stationsType,
+    "5sterren": stationsType
+};
+
+type stationsType = {
+    "name": string,
+    "audiostream": string,
+    "playlist-api": string,
+    "guide-api": string,
+    "img": string,
+    "themeColour": string,
+    "themeFilter": string,
+    "discordImg": string,
+    "modifier": number,
+    "sassName": string,
+    "font": string[]
+};
+
+type stationNames = "NPORadio1"|"NPORadio2"|"NPO3FM"|"NPORadio4"|"NPORadio5"|"NPOFunX"|"2S&J"|"3FMalternative"|"3FMkx"|"4Concerten"|"5sterren";
 /* === Onload functions == */
 
 //set up settings config
@@ -122,14 +154,35 @@ function settingsTab(e: Event) {
 
 //loads timetable
 function loadTimetable(fromOtherTab: boolean) {
-    var table: String[][] = settings.getSync("table");
+    var table: stationNames[][] = settings.getSync("table");
     var container: HTMLElement = document.querySelector('.settings [data-coupledid="timetable"] .container');
-    var tablehtml = fs.readFileSync("table.html", {encoding: "utf8"});
+    var tablehtml = fs.readFileSync(path.join(__dirname, "/dat/table.html"), {encoding: "utf8"});
     container.innerHTML = tablehtml;
-    table.forEach((e: String[], i: number) => {
+    table.forEach((e: stationNames[], i: number) => {
+        let previousStation: stationNames;
+        let samestationCounter: number = 0;
         var list: Element = container.children[i];
-        e.forEach((f: string, j: number)=> {
-            list.children[j + 1].classList.add(stations[f].sassName)
+        e.forEach((f: stationNames, j: number)=> {
+            if(f === previousStation) {
+                samestationCounter++;
+                if(samestationCounter >= 3) {
+                    let start: HTMLElement = <HTMLElement>list.children[j + 1];
+                    start.classList.add("img");
+                    start.style.backgroundPosition = "center " + (4-((4/samestationCounter+1)*(0))*-1).toString + "rem";
+                    for (let k = 0; k < samestationCounter; k++) {
+                        let currentEl: HTMLElement = <HTMLElement>start.previousElementSibling;
+                        start = currentEl;
+                        start.classList.add("img");
+                        let offset: number = 4-((4/samestationCounter+1)*(k+1))*-1;
+                        start.style.backgroundPosition = "center " + offset + "rem";
+                    }
+                }
+            }
+            else {
+                samestationCounter = 0;
+            }
+            list.children[j + 1].classList.add(stations[f].sassName, "deSat");
+            previousStation = f;
         })
     })
 };
